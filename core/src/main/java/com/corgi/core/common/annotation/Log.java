6aeb9783a1f0c6513b7616d014687e19de87559b
dengmiao <1297320571@qq.com>
2019-04-10 16:54:41 +0800

    日志

---
+++ core/src/main/java/com/corgi/core/common/annotation/Log.java
@@ -0,0 +1,42 @@
+package com.corgi.core.common.annotation;
+
+import com.corgi.core.common.constant.CommonConstant;
+
+import java.lang.annotation.*;
+
+/**
+ * @description: 日志注解
+ * @author: dengmiao
+ * @create: 2019-04-10 15:51
+ **/
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface Log {
+
+    /**
+     * 模块
+     */
+    String model();
+
+    /**
+     * 操作类型
+     */
+    String action();
+
+    /**
+     * 日志类型
+     * 0:操作日志;1:登录日志;2:定时任务;
+     */
+    int logType() default CommonConstant.LogType.LOGIN;
+
+    /**
+     * 操作人类别
+     */
+    int operatorType() default CommonConstant.OperatorType.MANAGE;
+
+    /**
+     * 是否保存请求的参数
+     */
+    boolean isSaveRequestData() default true;
+}
--- core/src/main/java/com/corgi/core/common/constant/CommonConstant.java
+++ core/src/main/java/com/corgi/core/common/constant/CommonConstant.java
@@ -7,7 +7,78 @@ package com.corgi.core.common.constant;
  **/
 public interface CommonConstant {
 
+    //=========================响应状态码==========================//
+    /**
+     * 成功
+     */
     Integer SC_OK_200 = 200;
 
+    /**
+     * 服务器错误
+     */
     Integer SC_INTERNAL_SERVER_ERROR_500 = 500;
+
+    //=========================系统日志==========================//
+    /**
+     * 操作类型
+     */
+    interface LogAction {
+        /**
+         * 新增
+         */
+        String ADD = "0";
+        /**
+         * 修改
+         */
+        String UPDATE = "1";
+        /**
+         * 添加或修改
+         */
+        String ADD_UPDATE = "2";
+        /**
+         * 删除
+         */
+        String DEL = "3";
+        /**
+         * 打开form
+         */
+        String OPEN_FORM = "4";
+        /**
+         * 查看form
+         */
+        String VIEW_FORM = "5";
+        /**
+         * form 添加或查看
+         */
+        String OPEN_VIEW_FORM = "6";
+        /**
+         * 查询list
+         */
+        String SELECT_LIST = "7";
+
+        /**
+         * 查询PAGE
+         */
+        String SELECT_PAGE = "8";
+    }
+
+    /**
+     * 日志类别
+     */
+    interface LogType {
+        /**
+         * 登录/操作/调度任务
+         */
+        int LOGIN = 0, OPERATE = 2, scheduled = 2;
+    }
+
+    /**
+     * 日志操作人类别
+     */
+    interface OperatorType {
+        /**
+         * 后台用户/手机用户/其他
+         */
+        int MANAGE = 0, MOBILE = 1, OTHER = 2;
+    }
 }
