6d2f48cdbbf8cf0dfe86a05b58c481356a265753
dengmiao <1297320571@qq.com>
2019-04-19 09:14:45 +0800

    多模块

---
+++ base/src/main/java/com/corgi/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.corgi.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author dengmiao
+ */
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
