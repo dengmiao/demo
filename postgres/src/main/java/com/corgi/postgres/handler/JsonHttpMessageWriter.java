364a3554390afec89d33641258675be359a308eb
dengmiao <1297320571@qq.com>
2019-05-08 12:40:31 +0800

    spring5 异常处理

---
+++ postgres/src/main/java/com/corgi/postgres/handler/JsonHttpMessageWriter.java
@@ -0,0 +1,51 @@
+package com.corgi.postgres.handler;
+
+import org.reactivestreams.Publisher;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.MediaType;
+import org.springframework.http.ReactiveHttpOutputMessage;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @title: JsonHttpMessageWriter
+ * @description: 处理json异常消息的类
+ * @author: dengmiao
+ * @create: 2019-05-08 11:47
+ **/
+@Component
+public class JsonHttpMessageWriter implements HttpMessageWriter<Map<String, Object>> {
+
+    @Override
+    public List<MediaType> getWritableMediaTypes() {
+        return Collections.singletonList(MediaType.APPLICATION_JSON_UTF8);
+    }
+
+    @Override
+    public boolean canWrite(ResolvableType resolvableType, MediaType mediaType) {
+        return MediaType.APPLICATION_JSON.includes(mediaType);
+    }
+
+    @Override
+    public Mono<Void> write(Publisher<? extends Map<String, Object>> publisher, ResolvableType resolvableType, MediaType mediaType, ReactiveHttpOutputMessage reactiveHttpOutputMessage, Map<String, Object> map) {
+        return Mono.from(publisher).flatMap(m -> reactiveHttpOutputMessage.writeWith(Mono.just(reactiveHttpOutputMessage.bufferFactory().wrap(transform2Json(m).getBytes()))));
+    }
+
+    private String transform2Json(Map<String, Object> sourceMap) {
+        return "{" +
+                "\"code\":" +
+                sourceMap.getOrDefault("code", 500) +
+                "," +
+                "\"msg\":\"" +
+                sourceMap.getOrDefault("msg", "") +
+                "\"," +
+                "\"data\":\"" +
+                sourceMap.getOrDefault("data", "") +
+                "\"}";
+    }
+}
